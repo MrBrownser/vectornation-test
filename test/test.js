cba8c7236b9c3de3732a1d156a58a4db373157ea
Adria Castany <adria.castany@haufe.com>
2021-05-07 06:37:29 +0200

    Set up tests config

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+
+describe('First block', function() {
+  it('First test case', function() {
+    expect(1+1).to.equal(2);
+  });
+});
